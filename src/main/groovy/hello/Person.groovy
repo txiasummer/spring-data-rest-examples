4a4212014bdd39dbd09f6c3d3cf04487b7547018
Tracy Xia <txiasummer@gmail.com>
2015-01-27 21:52:13 -0500

    commiting files

---
+++ src/main/groovy/hello/Person.groovy
@@ -0,0 +1,15 @@
+package hello
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id
+	String firstName
+	String lastName
+}

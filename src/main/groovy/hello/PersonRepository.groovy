537d2edf719814bb3fc201591629d51a6988c8fd
tracyxia <tracyxia@carfax.com>
2015-07-15 22:12:47 -0400

    Wrote ApiDocumentation spock test to auto-generate the web service documentation

--- src/main/groovy/hello/PersonRepository.groovy
+++ src/main/groovy/hello/PersonRepository.groovy
@@ -4,7 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource(collectionResourceRel="people", path="people")
+@RepositoryRestResource(collectionResourceRel='people', path='people')
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-	List<Person> findByLastName(@Param("name") String name)
+	List<Person> findByLastName(@Param('lastName') String lastName)
 }
--- src/test/groovy/hello/ApiDocumentation.groovy
+++ src/test/groovy/hello/ApiDocumentation.groovy
@@ -1,7 +1,66 @@
 package hello
 
+import org.springframework.boot.test.ConfigFileApplicationContextInitializer
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
+import javax.annotation.Resource
+import static org.springframework.restdocs.RestDocumentation.documentationConfiguration
+import static org.springframework.restdocs.RestDocumentation.document
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-
+@ContextConfiguration(classes = [Application], initializers = ConfigFileApplicationContextInitializer)
+@WebAppConfiguration
 class ApiDocumentation extends Specification {
+
+    @Resource
+    WebApplicationContext context
+
+    @Resource
+    PersonRepository personRepository
+
+    MockMvc mockMvc
+
+    void setup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).apply(documentationConfiguration()).build();
+        Person person1 = new Person(id: 1, firstName: 'Chewbacca', lastName: 'Xia')
+        Person person2 = new Person(id: 2, firstName: 'Furrs', lastName: 'Xia')
+        Person person3 = new Person(id: 3, firstName: 'Jameson', lastName: 'Parente')
+        Person person4 = new Person(id: 4, firstName: 'Milli', lastName: 'Brewer')
+        Person person5 = new Person(id: 5, firstName: 'Hooch', lastName: 'Brewer')
+
+        personRepository.save([person1, person2, person3, person4, person5])
+    }
+
+    void 'get all persons'(){
+        when:
+        ResultActions response = mockMvc.perform(get('/people'))
+
+        then:
+        response.andExpect(status().isOk())
+        response.andDo(document('person-list-example'))
+    }
+
+    void 'get specific person by id'(){
+        when:
+        ResultActions response = mockMvc.perform(get('/people/1'))
+
+        then:
+        response.andExpect(status().isOk())
+        response.andDo(document('person-by-id-example'))
+    }
+
+    void 'get specific person(s) by lastName'(){
+        when:
+        ResultActions response = mockMvc.perform(get('/people/search/findByLastName?lastName=Xia'))
+
+        then:
+        response.andExpect(status().isOk())
+        response.andDo(document('person-list-by-lastname'))
+    }
 }

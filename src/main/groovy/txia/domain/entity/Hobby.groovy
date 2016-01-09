9952b3c1db3c6d8306affcd14041b142c4b771be
Tracy Xia <txiasummer@gmail.com>
2016-01-08 22:24:01 -0500

    Modified Hobby.groovy to configure CascadeDelete against Personal_Hobby

--- src/main/groovy/txia/domain/entity/Hobby.groovy
+++ src/main/groovy/txia/domain/entity/Hobby.groovy
@@ -2,17 +2,26 @@ package txia.domain.entity
 
 import groovy.transform.Canonical
 
+import javax.persistence.CascadeType
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.OneToMany
 
 @Entity
 @Canonical
 class Hobby {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     Long id
+
     String name
+
     String description
+
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = 'hobby', cascade = CascadeType.REMOVE)
+    List<PersonalHobby> personalHobbies
 }
